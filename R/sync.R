1cd482daa9c7e9778bdbc3d0e1bfa80f605d30c3
Brandon Rose <thecodingdocs@gmail.com>
2025-01-16 01:19:21 -0500

    rename files

---
+++ R/sync.R
@@ -0,0 +1,63 @@
+sync <- function() {
+
+}
+sync_projects <- function() {
+
+}
+#' @title project_health_check
+#' @description
+#' Check directory, project object, and REDCap token. Optional update.
+#' @family Project Cache Functions
+#' @keywords Project Cache Functions
+#' @return project cache data.frame
+#' @export
+project_health_check <- function() {
+  # projects <- projects_old <- get_projects()
+  # DROPS <- NULL
+  # projects_old$test_dir <- FALSE
+  # projects$test_project <- FALSE
+  # projects$test_RC <- FALSE
+  # if(nrow(projects)>0){
+  #   # DROPS<- projects[which(is.na(projects$project_id)),]
+  #   for(i in seq_len(nrow(projects_old))){#i <- seq_len(nrow(projects))%>%  sample1()
+  #     OUT <- NULL
+  #     OUT <- projects_old[i,]
+  #     if(file.exists(OUT$dir_path)){
+  #       OUT$test_dir <- TRUE
+  #       project <- tryCatch({
+  #         load_project(OUT$dir_path)
+  #       },error = function(e) {NULL})
+  #       OUT$test_project <- !is.null(project)
+  #       if(!OUT$test_project){
+  #         project <- tryCatch({
+  #           setup_project(
+  #             short_name = OUT$short_name,
+  #             dir_path = OUT$dir_path,
+  #             token_name = OUT$token_name,
+  #             redcap_base = "https://redcap.miami.edu/",
+  #             reset = TRUE,
+  #             merge_form_name = "merged"
+  #           )
+  #         },error = function(e) {NULL})
+  #         OUT$test_project <- !is.null(project)
+  #       }
+  #       if(OUT$test_project){
+  #         OUT$test_RC <- redcap_token_works(project)
+  #         if(OUT$test_RC){
+  #           if(update)project <- sync_project(project)
+  #         }
+  #       }
+  #       if(OUT$test_project){
+  #         OUT_project <- extract_project_details(project = project)
+  #         OUT_project$test_dir <- OUT$test_dir
+  #         OUT_project$test_project <- OUT$test_project
+  #         OUT_project$test_RC <- OUT$test_RC
+  #         OUT <- OUT_project
+  #       }
+  #       projects <- projects[which(projects$short_name!=OUT$short_name),]
+  #       projects <- projects %>% dplyr::bind_rows(OUT)
+  #     }
+  #   }
+  #   save_projects_to_cache(projects,silent = FALSE)
+  # }
+}

23aef6451279ca9f3cc975811c7333ca22e86b51
brandonerose <brandon.ed.rose@gmail.com>
2026-03-09 13:27:46 -0500

    rename

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,15 @@
 #' @noRd
 .onLoad <- function(libname, pkgname) {
-  op <- options()
-  op.REDCapSync <- list(REDCapSync.header_style = .header_style,
-                        REDCapSync.body_style = .body_style,
-                        REDCapSync.show_REDCapR_messages = FALSE,
-                        REDCapSync.verbose = TRUE)
-  toset <- !(names(op.REDCapSync) %in% names(op))
-  if (any(toset)) {
-    options(op.REDCapSync[toset])
+  user_options <- options()
+  default_pkg_options <- list(
+    REDCapSync.header_style = .header_style,
+    REDCapSync.body_style = .body_style,
+    REDCapSync.show_REDCapR_messages = FALSE,
+    REDCapSync.verbose = TRUE
+  )
+  to_set <- !(names(default_pkg_options) %in% names(user_options))
+  if (any(to_set)) {
+    options(default_pkg_options[to_set])
   }
   get_cache()
   invisible()

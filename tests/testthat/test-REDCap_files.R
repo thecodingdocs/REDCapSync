0ab87ef7e36acf8a8409900c9acfc467ca2cd95f
brandonerose <brandon.ed.rose@gmail.com>
2026-03-08 23:00:56 -0500

    file renames

--- tests/testthat/test-REDCap_files.R
+++
@@ -1,2 +0,0 @@
-tempdir_file <- sanitize_path(withr::local_tempdir())
-withr::local_envvar("REDCAPSYNC_CACHE_OVERRIDE" = tempdir_file)
--- tests/testthat/test-utils-cli.R
+++
@@ -1,3 +0,0 @@
-# cli_message_maker ( Internal )
-test_that("cli_message_maker works!", {
-})

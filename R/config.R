e25032db6459e3235e778ef1162ffef0b2d15ec9
brandonerose <brandon.ed.rose@gmail.com>
2026-03-15 00:14:35 -0500

    config documentation

--- R/config.R
+++ R/config.R
@@ -1,42 +1,58 @@
-#' @title Configuration
-#' @description
-#' Configuration can be set with options as well as environment variables
+#' Package configuration accessors
+#'
+#' Internal configuration helpers used to retrieve package configuration
+#' values from options or environment variables.
+#'
+#' Configuration is resolved in the following order:
+#' 1. `options("redcapsync.config.*")`
+#' 2. `REDCAPSYNC_CONFIG_*` for logical and filepath types
+#' 3. Function default
+#'
+#' @details
+#' The `config` object is a list of accessor functions that retrieve
+#' configuration values and validate them.
+#'
+#' \describe{
+#'   \item{allow.test.names}{Logical. Whether `setup_project()` allows
+#'   project names starting with `"TEST_"`.}
+#'   \item{show.api.messages}{Logical. Whether to display API messages
+#'   returned by REDCapR.}
+#'   \item{verbose}{Logical. Controls verbosity of package messages.}
+#'   \item{cache.dir}{Character file path overriding the default cache
+#'   directory.}
+#'   \item{header.style}{`openxlsx` style object used for Excel header
+#'   formatting.}
+#'   \item{body.style}{`openxlsx` style object used for Excel body
+#'   formatting.}
+#' }
+#'
 #' @keywords internal
 config <- list(
-  #' @description allow.test.names is a logical for if [setup_project] will
-  #' allow projects that start with "TEST_", which in general will be reserved
-  #' for test fixture data. If the user wants to override this they can.
   allow.test.names = function(default = FALSE) {
     config_get(opt_name = "allow.test.names",
                type    = "logical",
                default = default)
   },
-  #' @description show.api.messages is a logical for showing REDCapR messages
   show.api.messages = function(default = FALSE) {
     config_get(opt_name = "show.api.messages",
                type = "logical",
                default = default)
   },
-  #' @description verbose is logical for how many messages to see in general
   verbose = function(default = TRUE) {
     config_get(opt_name = "verbose",
                type = "logical",
                default = default)
   },
-  #' @description cache.dir is a character file path if user wants to override
-  #' cache location.
   cache.dir = function (default = cache_path_default()){
     config_get(opt_name = "cache.dir",
                type = "filepath",
                default = default)
   },
-  #' @description header.style is a openxlsx style for the xlsx headers
   header.style = function(default = .header_style) {
     config_get(opt_name = "header.style",
                type = "openxlsx_style",
                default = default)
   },
-  #' @description header.style is a openxlsx style for the xlsx body
   body.style = function(default = .body_style) {
     config_get(opt_name = "body.style",
                type = "openxlsx_style",

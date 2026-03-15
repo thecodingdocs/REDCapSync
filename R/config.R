9e27ecf53409eb8db02947286a8ef0c81055e0df
brandonerose <brandon.ed.rose@gmail.com>
2026-03-14 23:37:10 -0500

    test desc

--- R/config.R
+++ R/config.R
@@ -1,34 +1,42 @@
-#' @title config
+#' @title Configuration
 #' @description
-#' configuration
-#'
+#' Configuration can be set with options as well as environment variables
 #' @keywords internal
 config <- list(
+  #' @description allow.test.names is a logical for if [setup_project] will
+  #' allow projects that start with "TEST_", which in general will be reserved
+  #' for test fixture data. If the user wants to override this they can.
   allow.test.names = function(default = FALSE) {
     config_get(opt_name = "allow.test.names",
                type    = "logical",
                default = default)
   },
+  #' @description show.api.messages is a logical for showing REDCapR messages
   show.api.messages = function(default = FALSE) {
     config_get(opt_name = "show.api.messages",
                type = "logical",
                default = default)
   },
+  #' @description verbose is logical for how many messages to see in general
   verbose = function(default = TRUE) {
     config_get(opt_name = "verbose",
                type = "logical",
                default = default)
   },
+  #' @description cache.dir is a character file path if user wants to override
+  #' cache location.
   cache.dir = function (default = cache_path_default()){
     config_get(opt_name = "cache.dir",
                type = "filepath",
                default = default)
   },
+  #' @description header.style is a openxlsx style for the xlsx headers
   header.style = function(default = .header_style) {
     config_get(opt_name = "header.style",
                type = "openxlsx_style",
                default = default)
   },
+  #' @description header.style is a openxlsx style for the xlsx body
   body.style = function(default = .body_style) {
     config_get(opt_name = "body.style",
                type = "openxlsx_style",
